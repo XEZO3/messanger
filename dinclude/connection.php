52c87a877e3052b410a6619d3e2120f667b73351
XEZO3 <75046635+XEZO3@users.noreply.github.com>
2021-07-06 00:10:21 +0300

    Delete dinclude directory

--- dinclude/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$dbname ="messanger";
-$username = "root";
-$password = "";
-$dsn = "mysql:host =$host;dbname=$dbname";
-$con= new PDO ($dsn,$username,$password);
-?>
--- dinclude/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include("connection.php");
-include("message.php");
-include("user-active.php");
-include("db.php");
-?>
